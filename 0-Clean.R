72d9c0f1e4e3c59c6ca99d311f4197afe79cf944
Vasanth <vasanth.g@adomita.com>
2017-10-23 15:42:44 +0530

    corrected the code

--- 0-Clean.R
+++ 0-Clean.R
@@ -9,7 +9,7 @@ storms <- storms %>%
   mutate(time = ymd_h(paste(year, month, day, hour))) %>% 
   select(name, year, time, lat, long, pressure, wind, type)
 
-storms <- storms %>% filter(type != "Extratropical")
+
 
 write.csv(storms, file = "storms.csv", row.names = FALSE)
   
